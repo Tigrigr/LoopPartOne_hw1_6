69b90ab81c116f741940794c3d8f8c0cfab1a7bf
mikha <Mikhailov.dzenphone@gmail.com>
2023-04-05 14:09:36 +0300

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
     public static void task1 () {
         System.out.println("Задача 1");
@@ -21,6 +22,13 @@ public class Main {
         System.out.println();
         System.out.println("Задача 3");
         for (int i = 0; i < 18; i = i+2){
+            System.out.println(i);
+        }
+    }
+    public static void task4 () {
+        System.out.println();
+        System.out.println("Задача 4");
+        for (int i = 10; i > -11; i--){
             System.out.println(i);
         }
     }
